aca9695db9e83ad087a783c632182e1b1cfbf574
Johan  Mufuta <jmufuta2@illinois.edu>
2022-10-21 21:18:55 -0400

    merge

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App';
+import theme from './Theme';
 
-import App from './App.tsx';
-import {createRoot} from "react-dom/client";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
 
 root.render(
-    <React.StrictMode>
+    <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <App />
-    </React.StrictMode>,
+    </ThemeProvider>,
 );
