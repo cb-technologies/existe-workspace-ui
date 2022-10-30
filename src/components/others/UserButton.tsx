041b573adbadd37a22e1d280ce45808166a12b79
anthonybgg <tonybongungu@gmail.com>
2022-10-30 18:40:55 -0400

    User profile icon to the UI

---
+++ src/components/others/UserButton.tsx
@@ -0,0 +1,60 @@
+import Box from "@mui/material/Box";
+import Tooltip from "@mui/material/Tooltip";
+import {IconButton} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+const settings = ['Profile', 'Compte', 'Dashboard', 'Se déconnecter'];
+
+const User = () => {
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <div>
+            <Box sx={{ flexGrow: 0 } }>
+                <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                    </IconButton>
+                </Tooltip>
+                <Menu
+                    sx={{ mt: '45px' }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                >
+                    {settings.map((setting) => (
+                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                            <Typography textAlign="center">{setting}</Typography>
+                        </MenuItem>
+                    ))}
+                </Menu>
+            </Box>
+
+    </div>
+    );
+}
+
+export default User;
