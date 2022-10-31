c5e55fa669f2bd8242b1891730de84bfc08db669
anthonybgg <tonybongungu@gmail.com>
2022-10-30 23:22:17 -0400

    little users icons to show user profile

--- src/components/others/UserButton.tsx
+++ src/components/others/UserButton.tsx
@@ -25,7 +25,7 @@ const User = () => {
             <Box sx={{ flexGrow: 0 } }>
                 <Tooltip title="Open settings">
                     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                        <Avatar alt="Remy Sharp" src={process.env.PUBLIC_URL + '/assets/avatar.png'} />
                     </IconButton>
                 </Tooltip>
                 <Menu
