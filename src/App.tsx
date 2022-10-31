8b1399a81ce453cfa0b497995692c29ab5a384df
anthonybgg <tonybongungu@gmail.com>
2022-10-30 23:19:51 -0400

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import {CssBaseline} from "@material-ui/core";
+import { CssBaseline} from "@material-ui/core";
 import {makeStyles} from "@mui/styles";
 import Header from "./components/others/Header";
 
-const useStyles = makeStyles((theme) => ({
-     root: {
+const useStyles = makeStyles(() => ({
+    root: {
         minHeight: '100vh',
         backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
         backgroundRepeat: 'no-repeat',
