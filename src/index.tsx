f7a9e3dd22c08451974a71ee4251f8558032cb94
Johan  Mufuta <jmufuta2@illinois.edu>
2022-11-02 01:01:14 -0400

    Landing_page with mui version

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
-import App from './App';
+// import App from './App';
 import theme from './Theme';
+// import Login from './components/pages/Login';
+import SignIn from './components/pages/Login2';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
@@ -12,6 +14,6 @@ root.render(
     <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <App />
+        <SignIn />
     </ThemeProvider>,
 );
