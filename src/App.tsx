c3b82f1db26acdd8cd5d4e8e4351c8c838ce9421
nicolasnk <nkierenicolas@gmail.com>
2022-11-05 17:20:59 -0700

    Pushing so Elie will work on it

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import { ExistCRUDClient } from './grpc/pb/Message_and_serviceServiceClientPb';
 import { NationalIDNumber, PersonInfoResponse } from './grpc/pb/message_and_service_pb';
 import * as grpcWeb from 'grpc-web';
 import SignUp from './components/pages/SignUp';
+import UpdateUserForm from './components/pages/updateUserInfo';
+import { Box, Container } from '@mui/material';
 
 export default function App() {
   return (
-    <SignUp />
+    <Container maxWidth="sm">
+      <Box sx={{my:8}}>
+        <UpdateUserForm />
+      </Box>
+    </Container>
   );
 }
