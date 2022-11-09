5218cce7af8229136c01bcfd668dc6a5d05b656c
masanka2 <masanka2@illinois.edu>
2022-11-08 22:11:47 -0800

    Update style for register and update

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import MainRouter from './components/pages/MainRouter';
 
 
+
 export default function App() {
   return (    
-      <MainRouter />
+      <MainRouter />  
   );
 }
