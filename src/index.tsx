5848c87ebfae205aaa7f1e9da04385feaa3cff6e
anthonybgg <tonybongungu@gmail.com>
2022-11-19 12:08:36 -0500

    landing page done and button redirect on proper page

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,21 @@
 import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
+import {ThemeProvider} from '@mui/material/styles';
 import App from './App';
 import theme from './Theme';
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import ResponsiveAppBar from "./components/pages/AppBar"
-
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 
 root.render(
     <BrowserRouter>
-    <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <ResponsiveAppBar/>
-        <App/>
-    </ThemeProvider>,
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <ResponsiveAppBar/>
+            <App/>
+        </ThemeProvider>,
     </BrowserRouter>
-
 );
