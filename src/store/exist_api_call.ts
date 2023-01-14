097e8d9803a68e209933b6275ff8d34b63413abc
nicolasnk <nkierenicolas@gmail.com>
2023-01-13 18:00:26 -0800

    Updated the webapp to call the public ec2 endpoint

--- src/store/exist_api_call.ts
+++ src/store/exist_api_call.ts
@@ -2,4 +2,4 @@ import { ExistCRUDClient } from "../grpc/pb/Message_and_serviceServiceClientPb";
 
 
 
-export const ExistService = new ExistCRUDClient("http://18-207-221-21:4551/", null, null);
+export const ExistService = new ExistCRUDClient("https://18-207-221-21:4551/", null, null);
