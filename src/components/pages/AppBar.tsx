1ba12bfe7011445158159bc5b67793a2f0a20b5f
anthonybgg <tonybongungu@gmail.com>
2023-01-24 21:41:40 -0500

    menu button

--- src/components/pages/AppBar.tsx
+++ src/components/pages/AppBar.tsx
@@ -18,57 +18,34 @@ import { Auth } from 'aws-amplify';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { URLExistPath } from "../../constants/existUrlPath";
 import { useEffect, useState } from 'react';
+import { AuthContext } from '../../store/auth_context';
 import {CssBaseline} from "@mui/material";
 
 const pages = [''];
 const settings = ['Logout'];
-// const settings = [
-//   {
-//     title: "Profile",
-//     page: URLExistPath.RegisterPage,
-//     flag: "to_register",
-//   },
-//   {
-//     buttonText: "Actualiser Un Citoyen",
-//     buttonVariant: "outlined",
-//     icon: BrowserUpdatedIcon,
-//     page: URLExistPath.RetrieveUserInfo,
-//     flag: "to_update",
-//   },
-//   {
-//     buttonText: "Generer carte d'identitée",
-//     buttonVariant: "outlined",
-//     icon: PermIdentityIcon,
-//     page: URLExistPath.RetrieveUserInfo,
-//     flag: "to_generate",
-//   },
-// ];
-
-
-
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
-  
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-  const navigate = useNavigate();
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const navigate = useNavigate();
 
   const navigateTo = (page: string, flag: string) => {
     navigate(page,{ state: { flag_to_page: flag } });
   };
-  
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [role, setRole] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -76,67 +53,7 @@ function ResponsiveAppBar() {
   const [prenom, setPrenom] = useState("");
   const [user, setUser] = useState(null);
 
-  // Auth.currentAuthenticatedUser().then(user => setUser(user))
-  //   .catch(() => setUser(null));
-
-  // Auth.onAuthUIStateChange((nextAuthState, authData) => {
-  //     if (nextAuthState === 'signedIn') {
-  //       // the user is signed in
-  //       setUser(authData.user)
-  //     } else if (nextAuthState === 'signedOut') {
-  //       // the user is signed out
-  //       setUser(null)
-  //     }
-  // });
-
-  // Auth.currentAuthenticatedUser()
-  // .then(user => {
-  //   console.log(user)
-  //   setIsLoggedIn(true);
-  //   setRole(user.attributes['custom:role'])
-  //   setNom(user.attributes['custom:nom'])
-  //   setPrenom(user.attributes['custom:prenom'])
-  //   setPhoneNumber(user.attributes['custom:phonenumber'])
-  // })
-  // .catch(err => {
-  //   console.log(err)
-  //   console.log("Petage")
-  //   setIsLoggedIn(false);
-  //   navigateTo(URLExistPath.SignInPage, "to_sign_in");
-  // });
-
-  // Auth.onAuthStateChange(user => {
-  //   if (user) {
-  //     console.log('User is signed in')
-  //   } else {
-  //     console.log('User is signed out')
-  //   }
-  // })
-
-  useEffect(() => {
-    console.log("arriviO yeba tseng")
-    console.log(isLoggedIn)
-    async function checkAuth() {
-      try {
-        const user = await Auth.currentUserInfo();
-        console.log("arriving")
-        setIsLoggedIn(true);
-      } catch {
-        console.log("Petage")
-        setIsLoggedIn(false);
-      }
-    }
-    checkAuth();
-  }, [isLoggedIn]);
-
-  const signOut = async () => {
-    try {
-      await Auth.signOut();
-      navigate(URLExistPath.HomePage);
-    } catch (error) {
-        console.log('error signing out: ', error);
-    }
-  };
+  const authContext = React.useContext(AuthContext);
 
   return (
     <AppBar position="static" color='transparent'>
@@ -180,7 +97,7 @@ function ResponsiveAppBar() {
 
           {/* in case loggedIn */}
           {<div>
-            {isLoggedIn?
+            {authContext.isAuthenticated?
             <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
@@ -205,7 +122,7 @@ function ResponsiveAppBar() {
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting) => (
-                <MenuItem key={setting} onClick={signOut}>
+                <MenuItem key={setting} onClick={authContext.logout}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
@@ -215,7 +132,7 @@ function ResponsiveAppBar() {
             null
             }
             </div>}
-          
+
         </Toolbar>
       </Container>
     </AppBar>
