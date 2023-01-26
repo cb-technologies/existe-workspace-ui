aa0118ab958c7fc2b22b52c26660ba36368fd0a2
anthonybgg <tonybongungu@gmail.com>
2023-01-26 02:45:57 -0500

    added retrieve with qr code encrypted

--- src/components/pages/IDCard.tsx
+++ src/components/pages/IDCard.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {Box, Button, Container} from "@mui/material";
-import {Link, useLocation} from "react-router-dom";
+import { Container, Box, Button } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
 import CardBack from "./CardBack";
-import {PersonInfoResponse} from "../../grpc/pb/message_and_service_pb";
+import { PersonInfoResponse } from "../../grpc/pb/message_and_service_pb";
 import CardFront from "./CardFront";
 
 export default function IDCard() {
@@ -17,11 +17,11 @@ export default function IDCard() {
                 </Box>
             </Container>
             <Box sx={{my: 8, width: "80%", boxShadow: 15, borderRadius: 3}}>
-                <CardBack userInfo={userInfo}/>
-                <Link to="/" style={{textDecoration: "none"}}>
-                    <Button variant="contained">Go Back</Button>
-                </Link>
-            </Box>
-        </Container>
-    );
+                <CardBack userInfo={userInfo} />
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <Button variant="contained">Go Back</Button>
+        </Link>
+      </Box>
+    </Container>
+  );
 }
