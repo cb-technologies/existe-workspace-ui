29a5e142d8a3d9b755ae1a56a76b0a999d1a8cda
nicolasnk <nkierenicolas@gmail.com>
2023-02-24 15:34:28 -0800

    Updated the backend endpoint to the elastic IP address

--- src/store/exist_api_call.ts
+++ src/store/exist_api_call.ts
@@ -1,7 +1,7 @@
 import { ExistCRUDClient } from "../grpc/pb/Message_and_serviceServiceClientPb";
 
 export const ExistService = new ExistCRUDClient(
-  "https://52.70.141.185:4551/",
+  "https://34.194.131.22:4551/",
   null,
   null
 );
