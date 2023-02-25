35d4252a5cd1a31f5200c870046e156b047c628b
nicolasnk <nkierenicolas@gmail.com>
2023-02-24 17:22:06 -0800

    Change our backend endpoint to be a domain. THis PR is in conjuction with the change that have been made in AWS and our EC2

--- src/store/exist_api_call.ts
+++ src/store/exist_api_call.ts
@@ -1,7 +1,7 @@
 import { ExistCRUDClient } from "../grpc/pb/Message_and_serviceServiceClientPb";
 
 export const ExistService = new ExistCRUDClient(
-  "https://52.70.141.185:4551/",
+  "https://existeid-go-server.com:4551/",
   null,
   null
 );
